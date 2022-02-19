00db968e92647eacf3d7fa2505168219c328428e
Denis Dolbilin <kombogame6@gmail.com>
2022-02-19 19:25:59 +0200

    feat: added navbar, will do some styling next

--- src/shared/ui/nav-bar/nav-bar.tsx
+++ src/shared/ui/nav-bar/nav-bar.tsx
@@ -1,10 +1,19 @@
 import { Typography } from '@mui/material';
+import { NavLink } from '..';
 import styles from './nav-bar.module.css';
 
 export function NavBar() {
+  const links = [
+    <NavLink linkTo="" title="Турниры" />,
+    <NavLink linkTo="" title="Команды" />,
+  ];
+
   return (
     <div className={styles.container}>
-      <Typography variant="h1">h1. Heading</Typography>
+      <Typography variant="h5" align="left" sx={{ padding: '15px' }}>
+        Heading
+      </Typography>
+      {links.map(item => item)}
     </div>
   );
 }
--- src/shared/ui/nav-link/index.ts
+++ src/shared/ui/nav-link/index.ts
@@ -1,2 +1,1 @@
-export { NavBar } from './nav-bar';
 export { NavLink } from './nav-link';
