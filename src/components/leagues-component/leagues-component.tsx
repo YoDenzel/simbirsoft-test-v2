df96af14f3585d51ce26ef4492ab0bc87b3165c1
Denis Dolbilin <kombogame6@gmail.com>
2022-02-25 15:28:27 +0200

    feat: added pagination

--- src/components/leagues-component/leagues-component.tsx
+++ src/components/leagues-component/leagues-component.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextField } from '@mui/material';
+import { Pagination, TextField } from '@mui/material';
 import { useGetLeaguesList } from '../../shared/custom-hooks';
 import styles from './leagues-component.module.css';
 import { LeagueElement } from '../../shared/ui';
@@ -7,7 +7,19 @@ import { LeagueElement } from '../../shared/ui';
 export function LeaguesComponent() {
   const [search, setSearch] = useState('');
   const { data, isError } = useGetLeaguesList();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(12);
+
   const PLACEHOLDER = 'Поиск';
+
+  const pageCount = Math.ceil((data?.count || 0) / postsPerPage);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = data?.competitions.slice(
+    indexOfFirstPost,
+    indexOfLastPost,
+  );
 
   console.log(data, isError);
 
@@ -25,13 +37,20 @@ export function LeaguesComponent() {
         }}
       />
       <div className={styles.list_block}>
-        {data?.competitions.map(item => (
+        {currentPosts?.map(item => (
           <LeagueElement
             key={item.id}
             countryName={item.area.name}
             leagueName={item.name}
           />
         ))}
+      </div>
+      <div className={styles.pagination_block}>
+        <Pagination
+          count={pageCount}
+          onChange={(event, value) => setCurrentPage(value)}
+          shape="rounded"
+        />
       </div>
     </div>
   );
