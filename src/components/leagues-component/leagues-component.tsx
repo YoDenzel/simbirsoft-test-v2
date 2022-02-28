a2ce8b20245a5f219a1f4dafb80eb18ac623afa2
Denis Dolbilin <kombogame6@gmail.com>
2022-02-28 21:51:13 +0200

    refactor: replaced pagination and local storage code with custom hooks

--- src/components/leagues-component/leagues-component.tsx
+++ src/components/leagues-component/leagues-component.tsx
@@ -1,27 +1,21 @@
-import { useEffect, useState } from 'react';
 import { Pagination, TextField } from '@mui/material';
-import { useGetData } from '../../shared/custom-hooks';
+import {
+  useGetData,
+  useLocalStorage,
+  usePagination,
+} from '../../shared/custom-hooks';
 import styles from './leagues-component.module.css';
 import { LeagueElement } from '../../shared/ui';
 import { TCompetitionsList } from '../../shared/types';
 
 export function LeaguesComponent() {
-  const [search, setSearch] = useState(() => {
-    const value = localStorage.getItem('search');
-    return value || '';
+  const [search, setSearch] = useLocalStorage({
+    defaultValue: '',
+    key: 'search',
   });
   const { data, isError } = useGetData('leagues', 'competitions/');
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(12);
 
   const PLACEHOLDER = 'Поиск';
-
-  useEffect(() => {
-    if (search.length > 0) {
-      setCurrentPage(1);
-    }
-    localStorage.setItem('search', search);
-  }, [search]);
 
   const filteredData = data?.competitions.filter((item: TCompetitionsList) => {
     if (
@@ -31,10 +25,9 @@ export function LeaguesComponent() {
     }
     return null;
   });
-  const pageCount = Math.ceil((filteredData?.length || 0) / postsPerPage);
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = filteredData?.slice(indexOfFirstPost, indexOfLastPost);
+  const { currentPosts, pageCount, setCurrentPage } = usePagination({
+    filteredData,
+  });
 
   console.log(data, isError);
 
