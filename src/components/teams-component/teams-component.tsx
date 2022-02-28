4cc4cf2eb4687742e1460d32c5acbfdb2317cd2e
Denis Dolbilin <kombogame6@gmail.com>
2022-03-01 00:28:08 +0200

    feat: added component for teams

---
+++ src/components/teams-component/teams-component.tsx
@@ -0,0 +1,62 @@
+import { Pagination, TextField } from '@mui/material';
+import {
+  useGetData,
+  useLocalStorage,
+  usePagination,
+} from '../../shared/custom-hooks';
+import { TTeams } from '../../shared/types';
+import { LeagueElement } from '../../shared/ui';
+import styles from '../leagues-component/leagues-component.module.css';
+
+export function TeamsComponent() {
+  const { data } = useGetData('teams', 'teams/');
+  const [search, setSearch] = useLocalStorage({
+    defaultValue: '',
+    key: 'search',
+  });
+  const PLACEHOLDER = 'Поиск';
+  const filteredData = data?.teams.filter((item: TTeams) => {
+    if (
+      item.name.toUpperCase().includes(search.toUpperCase().replace(/\s/g, ''))
+    ) {
+      return item;
+    }
+    return null;
+  });
+  const { currentPosts, pageCount, setCurrentPage } = usePagination({
+    filteredData,
+  });
+
+  console.log(data);
+  return (
+    <div className={styles.container}>
+      <TextField
+        id="outlined-basic"
+        placeholder={PLACEHOLDER}
+        variant="outlined"
+        value={search}
+        size="medium"
+        onChange={event => setSearch(event.target.value)}
+        sx={{
+          margin: '20px 16px',
+        }}
+      />
+      <div className={styles.list_block}>
+        {currentPosts?.map((item: TTeams) => (
+          <LeagueElement
+            key={item.id}
+            countryName={item.area.name}
+            leagueName={item.name}
+          />
+        ))}
+      </div>
+      <div className={styles.pagination_block}>
+        <Pagination
+          count={pageCount}
+          onChange={(_, value) => setCurrentPage(value)}
+          shape="rounded"
+        />
+      </div>
+    </div>
+  );
+}
