0017df86bfbd4b49ff923efc3d961b62831a46a0
Denis Dolbilin <kombogame6@gmail.com>
2022-03-04 19:38:20 +0200

    feat: added newUrl for filtration

--- src/shared/custom-hooks/use-get-data/use-get-data.ts
+++ src/shared/custom-hooks/use-get-data/use-get-data.ts
@@ -1,12 +1,38 @@
+import { format } from 'date-fns';
 import { useQuery } from 'react-query';
 import { fetchData } from '../../api';
 
-const STALE_TIME = 60000;
+const STALE_TIME = 0;
 
-export const useGetData = (QUERY_KEY: string, url: string) => {
-  const query = useQuery(QUERY_KEY, () => fetchData(url), {
-    retry: 0,
-    staleTime: STALE_TIME,
-  });
+type TProps = {
+  QUERY_KEY: string;
+  url: string;
+  firstValue?: Date | null;
+  secondValue?: Date | null;
+};
+
+export const useGetData = ({
+  QUERY_KEY,
+  url,
+  firstValue,
+  secondValue,
+}: TProps) => {
+  let firstFormatedValue = null;
+  let secondFormatedValue = null;
+  let filteredUrl = url;
+  if (firstValue && secondValue && firstValue < secondValue) {
+    firstFormatedValue = format(firstValue, 'yyyy-MM-dd');
+    secondFormatedValue = format(secondValue, 'yyyy-MM-dd');
+    filteredUrl += `?dateFrom=${firstFormatedValue}&dateTo=${secondFormatedValue}`;
+    console.log(filteredUrl);
+  }
+  const query = useQuery(
+    [QUERY_KEY, filteredUrl],
+    () => fetchData(filteredUrl),
+    {
+      retry: 0,
+      staleTime: STALE_TIME,
+    },
+  );
   return query;
 };
