d055512c5d5f941eec9da7e2b3d31b2ce5f46f66
Denis Dolbilin <kombogame6@gmail.com>
2022-03-04 22:35:05 +0200

    feat: new exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { LeaguesComponent } from './leagues-component';
 export { TeamsComponent } from './teams-component';
 export { LeagueNameComponent } from './league-name-component';
+export { TeamNameComponent } from './team-name-component';
--- src/shared/custom-hooks/index.ts
+++ src/shared/custom-hooks/index.ts
@@ -1,3 +1,6 @@
 export { useGetData } from './use-get-data';
 export { useLocalStorage } from './use-local-storage';
 export { usePagination } from './use-pagination';
+export { useStatus } from './use-status';
+export { useTimeFormat } from './use-time-format';
+export { useGetMatchResult } from './use-get-match-result';
